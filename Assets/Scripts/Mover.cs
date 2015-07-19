b2265d9da0d2b4e54b66573cab73946438f25201
ABeloeil <abeloeil@u-pro.fr>
2015-07-19 21:43:06 +0200

    First commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour 
+{
+	public float speed;
+	void Start()
+	{
+		var rigidbody = GetComponent<Rigidbody> ();
+		rigidbody.velocity = transform.forward * speed;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+
+	public float fireRate;
+	private float nextFire;
+
+	void Update()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		var rigibody = GetComponent<Rigidbody> ();
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigibody.velocity = movement * speed;
+		rigibody.position = new Vector3 (
+			Mathf.Clamp(rigibody.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp(rigibody.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		rigibody.rotation = Quaternion.Euler (0.0f, 0.0f, rigibody.velocity.x * -tilt);
+	}
+}
