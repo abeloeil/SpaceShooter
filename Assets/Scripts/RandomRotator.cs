72d401a4b7d095553cb72ee25a3122bf291359f6
ABeloeil <abeloeil@u-pro.fr>
2015-07-20 22:08:34 +0200

    Add Asteroid object, with random rotation & DestroyOnContact script

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour 
+{
+	public float tumble;
+	void Start ()
+	{
+		var rigidbody = GetComponent<Rigidbody> ();
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
