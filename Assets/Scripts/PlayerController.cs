a6b65a7f1048d5b0b322823501b3c05e4f7abcff
ABeloeil <abeloeil@u-pro.fr>
2015-07-21 20:04:49 +0200

    add background music, explosion sound on asteroid, and laser shoots effect

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,6 +24,7 @@ public class PlayerController : MonoBehaviour
 		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
 			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+			GetComponent<AudioSource>().Play();
 		}
 	}
 
